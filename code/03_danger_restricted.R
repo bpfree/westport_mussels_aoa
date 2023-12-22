d4660636fb04c411884f56d2e74741fad9f624eb
bpfree <brian.patrick.free@gmail.com>
2023-12-22 10:51:23 -0500

    Clean code to create to have more comparable naming conventions

--- code/03_danger_restricted.R
+++ code/03_danger_restricted.R
@@ -46,7 +46,7 @@ pacman::p_load(docxtractr,
 ## define data directory (as this is an R Project, pathnames are simplified)
 ### input directories
 #### danger zones and restricted areas
-danger_restricted_gpkg <- "data/a_raw_data/DangerZoneRestrictedArea/DangerZoneRestrictedArea.gpkg"
+data_gpkg <- "data/a_raw_data/DangerZoneRestrictedArea/DangerZoneRestrictedArea.gpkg"
 
 #### study area grid
 study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
@@ -61,7 +61,7 @@ danger_gpkg <- "data/b_intermediate_data/westport_danger_restricted_zones.gpkg"
 #####################################
 
 # inspect layers within geodatabases and geopackages
-sf::st_layers(dsn = danger_restricted_gpkg,
+sf::st_layers(dsn = data_gpkg,
               do_count = T)
 
 sf::st_layers(dsn = study_region_gpkg,
@@ -90,8 +90,8 @@ date <- format(Sys.time(), "%Y%m%d")
 # load data
 ## danger zones and restricted areas data (source: https://marinecadastre.gov/downloads/data/mc/DangerZoneRestrictedArea.zip)
 ### metadata: https://www.fisheries.noaa.gov/inport/item/48876
-danger_zones <- sf::st_read(dsn = danger_restricted_gpkg,
-                            layer = sf::st_layers(danger_restricted_gpkg)[[1]]) %>%
+danger_zones <- sf::st_read(dsn = data_gpkg,
+                            layer = sf::st_layers(data_gpkg)[[1]]) %>%
   # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
   sf::st_transform(x = ., crs = crs)
 
