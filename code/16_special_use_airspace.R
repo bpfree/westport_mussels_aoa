e5efcbb3f7713b9b67c962d3a0c9720400beaaec
bpfree <brian.patrick.free@gmail.com>
2024-10-08 12:39:19 -0400

    special use airspace script

---
+++ code/16_special_use_airspace.R
@@ -0,0 +1,148 @@
+################################
+### 16. Special use airspace ###
+################################
+
+# clear environment
+rm(list = ls())
+
+# calculate start time of code (determine how long it takes to complete all code)
+start <- Sys.time()
+
+#####################################
+#####################################
+
+# load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(docxtractr,
+               dplyr,
+               elsa,
+               fasterize,
+               fs,
+               ggplot2,
+               janitor,
+               ncf,
+               paletteer,
+               pdftools,
+               plyr,
+               purrr,
+               raster,
+               RColorBrewer,
+               reshape2,
+               rgdal,
+               rgeoda,
+               rgeos,
+               rmapshaper,
+               rnaturalearth, # use devtools::install_github("ropenscilabs/rnaturalearth") if packages does not install properly
+               sf,
+               sp,
+               stringr,
+               terra, # is replacing the raster package
+               tidyr)
+
+#####################################
+#####################################
+
+# set parameters
+## designate region name
+region_name <- "westport"
+
+## coordinate reference system
+### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
+crs <- "EPSG:26918"
+
+## layer names
+layer_name <- "special_use_airspace"
+
+## submodel
+submodel <- "national_security"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### special use airspace
+data_dir <- "data/a_raw_data/MilitaryCollection/MilitaryCollection.gpkg"
+
+#### study area grid
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
+
+### output directories
+#### submodel
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
+# load data
+## special use airspace data (source: https://marinecadastre.gov/downloads/data/mc/MilitaryCollection.zip)
+### metadata: https://www.fisheries.noaa.gov/inport/item/48898
+data <- sf::st_read(dsn = data_dir,
+                                    # special use airspace
+                                    layer = st_layers(dsn = data_dir)[[1]][5]) %>%
+  # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
+  sf::st_transform(x = ., crs = crs)
+
+#####################################
+
+## study region
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_hex_rm_constraints_boundary_{date}"))
+
+## hex grid
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_hex_rm_constraints_{date}"))
+
+#####################################
+#####################################
+
+# limit data to study region
+region_data <- data %>%
+  # obtain only special use airspace in the study area
+  rmapshaper::ms_clip(target = .,
+                      clip = region) %>%
+  # create field called "layer" and fill with "special use airspace" for summary
+  dplyr::mutate(layer = "special use airspace")
+
+#####################################
+#####################################
+
+# special use airspace hex grids
+region_data_hex <- hex_grid[region_data, ] %>%
+  # spatially join special use airspace values to Westport hex cells
+  sf::st_join(x = .,
+              y = region_data,
+              join = st_intersects) %>%
+  # select fields of importance
+  dplyr::select(index, layer)
+
+#####################################
+#####################################
+
+# export data
+## submodel geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region_name}_hex_{layer_name}_{date}"), append = F)
+
+## data geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
+
+#####################################
+#####################################
+
+# calculate end time and print time difference
+print(Sys.time() - start) # print how long it takes to calculate
