121df03c89bc90d9c38d36c23f6f2784dafbf0cd
bpfree <brian.patrick.free@gmail.com>
2024-10-08 10:11:50 -0400

    move parameters; update pathnames and standardize object names

--- code/23_combined_protected_resources.R
+++ code/23_combined_protected_resources.R
@@ -42,6 +42,23 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
+# set parameters
+## designate region name
+region_name <- "westport"
+
+## coordinate reference system
+### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
+crs <- "EPSG:26918"
+
+## layer names
+layer_name <- "combined_protected_resources"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
 # set directories
 ## define data directory (as this is an R Project, pathnames are simplified)
 ### input directories
@@ -49,37 +66,20 @@ pacman::p_load(docxtractr,
 data_dir <- "data/a_raw_data/combined_protected_resources"
 
 #### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
 
 ### output directories
 #### natural and cultural resources
 natural_cultural_gpkg <- "data/c_submodel_data/natural_cultural.gpkg"
 
 #### intermediate directories
-comb_prot_resources_gpkg <- "data/b_intermediate_data/westport_combined_protected_resources.gpkg"
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
 
 #####################################
 
 # inspect layers within geopackage
-sf::st_layers(dsn = study_region_gpkg,
+sf::st_layers(dsn = region_gpkg,
               do_count = T)
-
-#####################################
-#####################################
-
-# set parameters
-## designate region name
-region <- "westport"
-
-## coordinate reference system
-### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
-crs <- "EPSG:26918"
-
-## layer names
-export_name <- "combined_protected_resources"
-
-## designate date
-date <- format(Sys.Date(), "%Y%m%d")
 
 #####################################
 #####################################
@@ -127,10 +127,10 @@ comb_prot_resources <- sf::st_read(dsn = file.path(paste(data_dir, "final_PRD_CE
 #####################################
 
 ## study region
-westport_region <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area", sep = "_"))
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
 
 ## hex grid
-westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area_hex", sep = "_"))
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
 
 #####################################
 #####################################
@@ -139,7 +139,7 @@ westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area
 westport_comb_prot_resources <- comb_prot_resources %>%
   # obtain only combined protected resources in the study area
   rmapshaper::ms_clip(target = .,
-                      clip = westport_region) %>%
+                      clip = region) %>%
   # create field called "layer" and fill with "combined protected resources" for summary
   dplyr::mutate(layer = "combined protected resources")
 
@@ -147,7 +147,7 @@ westport_comb_prot_resources <- comb_prot_resources %>%
 #####################################
 
 # combined protected resources hex grids
-westport_comb_prot_resources_hex <- westport_hex[westport_comb_prot_resources, ] %>%
+westport_comb_prot_resources_hex <- hex_grid[westport_comb_prot_resources, ] %>%
   # spatially join combined protected resources values to Westport hex cells
   sf::st_join(x = .,
               y = westport_comb_prot_resources,
@@ -172,11 +172,11 @@ westport_comb_prot_resources_hex <- westport_hex[westport_comb_prot_resources, ]
 
 # export data
 ## constraints geopackage
-sf::st_write(obj = westport_comb_prot_resources_hex, dsn = natural_cultural_gpkg, layer = paste(region, "hex", export_name, date, sep = "_"), append = F)
+sf::st_write(obj = westport_comb_prot_resources_hex, dsn = natural_cultural_gpkg, layer = stringr::str_glue("{region}_hex_{layer_name}_{date}"), append = F)
 
 ## combined protected resources geopackage
-sf::st_write(obj = comb_prot_resources, dsn = comb_prot_resources_gpkg, layer = paste(export_name, date, sep = "_"), append = F)
-sf::st_write(obj = westport_comb_prot_resources, dsn = comb_prot_resources_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+sf::st_write(obj = comb_prot_resources, dsn = comb_prot_resources_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = westport_comb_prot_resources, dsn = comb_prot_resources_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
