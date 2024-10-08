a3a4c59ef1fc97aa5cacf8a8b18a4a57c3eebedd
bpfree <brian.patrick.free@gmail.com>
2024-10-08 11:37:27 -0400

    proposed offshore wind cable corridors

---
+++ code/13_proposed_offshore_wind_cable_corridors.R
@@ -0,0 +1,150 @@
+##################################################
+### 13. Proposed offshore wind cable corridors ###
+##################################################
+
+# clear environment
+rm(list = ls())
+
+# calculate start time of code (determine how long it takes to complete all code)
+start <- Sys.time()
+
+#####################################
+#####################################
+
+# load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(docxtractr,
+               dplyr,
+               elsa,
+               fasterize,
+               fs,
+               ggplot2,
+               janitor,
+               ncf,
+               paletteer,
+               pdftools,
+               plyr,
+               purrr,
+               raster,
+               RColorBrewer,
+               reshape2,
+               rgdal,
+               rgeoda,
+               rgeos,
+               rmapshaper,
+               rnaturalearth, # use devtools::install_github("ropenscilabs/rnaturalearth") if packages does not install properly
+               sf,
+               sp,
+               stringr,
+               terra, # is replacing the raster package
+               tidyr)
+
+#####################################
+#####################################
+
+# set parameters
+## designate region name
+region_name <- "westport"
+
+## coordinate reference system
+### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
+crs <- "EPSG:26918"
+
+## layer names
+layer_name <- "cable_corridors"
+
+# submodel
+submodel <- "constraints"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### cable corridors
+data_dir <- "data/a_raw_data/Offshore_Wind-_Proposed_Export_Cable_Corridors"
+
+#### study area grid
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
+
+### output directories
+#### submodel
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
+# load data
+## proposed offshore wind cable corridors
+### Marine Cadastre: https://hub.marinecadastre.gov/datasets/BOEM::offshore-wind-export-cable-corridors-proposed
+### metadata: https://www.arcgis.com/sharing/rest/content/items/0e57fcbb8aaf49c5b8d0944a4ffeef08/info/metadata/metadata.xml?format=default&output=html
+### REST server: https://services7.arcgis.com/G5Ma95RzqJRPKsWL/arcgis/rest/services/Offshore_Wind-_Proposed_Export_Cable_Corridors/FeatureServer/0
+
+data <- sf::st_read(dsn = file.path(data_dir, "Offshore_Wind-_Proposed_Export_Cable_Corridors.shp")) %>%
+  # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
+  sf::st_transform(x = ., crs = crs)
+
+#####################################
+
+## study region
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
+
+## hex grid
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
+
+#####################################
+#####################################
+
+# limit data to study region
+region_data <- data %>%
+  # obtain only cable corridors in the study area
+  rmapshaper::ms_clip(target = .,
+                      clip = region) %>%
+  # create field called "layer" and fill with "cable corridors" for summary
+  dplyr::mutate(layer = "cable corridors")
+
+#####################################
+#####################################
+
+# cable corridors hex grids
+region_data_hex <- hex_grid[region_data, ] %>%
+  # spatially join cable corridors values to Westport hex cells
+  sf::st_join(x = .,
+              y = region_data,
+              join = st_intersects) %>%
+  # select fields of importance
+  dplyr::select(index, layer) %>%
+  # group by the index values as there are duplicates
+  dplyr::group_by(index) %>%
+  # summarise by index
+  dplyr::summarise()
+
+#####################################
+#####################################
+
+# export data
+## submodel geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region_name}_hex_{layer_name}_{date}"), append = F)
+
+## data geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
+
+#####################################
+#####################################
+
+# calculate end time and print time difference
+print(Sys.time() - start) # print how long it takes to calculate
