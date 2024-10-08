f96329e10a75582c42067ea9404a7baed0824ee2
bpfree <brian.patrick.free@gmail.com>
2024-10-08 10:01:18 -0400

    move parameters; update pathnames and standardize object names

--- code/06_danger_restricted.R
+++ code/06_danger_restricted.R
@@ -42,44 +42,19 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
-# set directories
-## define data directory (as this is an R Project, pathnames are simplified)
-### input directories
-#### danger zones and restricted areas
-data_dir <- "data/a_raw_data/DangerZoneRestrictedArea/DangerZoneRestrictedArea.gpkg"
-
-#### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
-
-### output directories
-#### constraints
-constraints_gpkg <- "data/c_submodel_data/constraints.gpkg"
-
-#### intermediate directories
-danger_gpkg <- "data/b_intermediate_data/westport_danger_restricted_zones.gpkg"
-
-#####################################
-
-# inspect layers within geodatabases and geopackages
-sf::st_layers(dsn = data_dir,
-              do_count = T)
-
-sf::st_layers(dsn = study_region_gpkg,
-              do_count = T)
-
-#####################################
-#####################################
-
 # set parameters
 ## designate region name
-region <- "westport"
+region_name <- "westport"
 
 ## coordinate reference system
 ### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
 crs <- "EPSG:26918"
 
 ## layer names
-export_name <- "danger_zones_restricted_areas"
+layer_name <- "danger_zones_restricted_areas"
+
+## submodel
+submodel <- "constraints"
 
 ## designate date
 date <- format(Sys.Date(), "%Y%m%d")
@@ -87,10 +62,38 @@ date <- format(Sys.Date(), "%Y%m%d")
 #####################################
 #####################################
 
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### danger zones and restricted areas
+data_dir <- "data/a_raw_data/DangerZoneRestrictedArea/DangerZoneRestrictedArea.gpkg"
+
+#### study area grid
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
+
+### output directories
+#### submodel
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
 # load data
 ## danger zones and restricted areas data (source: https://marinecadastre.gov/downloads/data/mc/DangerZoneRestrictedArea.zip)
 ### metadata: https://www.fisheries.noaa.gov/inport/item/48876
-danger_zones <- sf::st_read(dsn = data_dir,
+data <- sf::st_read(dsn = data_dir,
                             layer = sf::st_layers(data_dir)[[1]][1]) %>%
   # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
   sf::st_transform(x = ., crs = crs)
@@ -98,19 +101,19 @@ danger_zones <- sf::st_read(dsn = data_dir,
 #####################################
 
 ## study region
-westport_region <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area", sep = "_"))
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
 
 ## hex grid
-westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area_hex", sep = "_"))
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
 
 #####################################
 #####################################
 
 # limit data to study region
-westport_danger_zones <- danger_zones %>%
+region_data <- data %>%
   # obtain only danger zones and restricted areas in the study area
   rmapshaper::ms_clip(target = .,
-                      clip = westport_region) %>%
+                      clip = region) %>%
   # create field called "layer" and fill with "danger zones and restricted areas" for summary
   dplyr::mutate(layer = "danger zones and restricted areas")
 
@@ -118,10 +121,10 @@ westport_danger_zones <- danger_zones %>%
 #####################################
 
 # danger zone and restricted area hex grids
-westport_danger_hex <- westport_hex[westport_danger_zones, ] %>%
+region_data_hex <- hex_grid[region_data, ] %>%
   # spatially join danger zones and restricted areas values to Westport hex cells
   sf::st_join(x = .,
-              y = westport_danger_zones,
+              y = region_data,
               join = st_intersects) %>%
   # select fields of importance
   dplyr::select(index, layer)
@@ -130,12 +133,12 @@ westport_danger_hex <- westport_hex[westport_danger_zones, ] %>%
 #####################################
 
 # export data
-## constraints geopackage
-sf::st_write(obj = westport_danger_hex, dsn = constraints_gpkg, layer = paste(region, "hex", export_name, date, sep = "_"), append = F)
+## submodel geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region_name}_hex_{layer_name}_{date}"), append = F)
 
-## danger zone and restricted area geopackage
-sf::st_write(obj = danger_zones, dsn = danger_gpkg, layer = paste(export_name, date, sep = "_"), append = F)
-sf::st_write(obj = westport_danger_zones, dsn = danger_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+## data geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
