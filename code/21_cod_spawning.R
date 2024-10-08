a9a4a184c34f9770470ba64393df5722f80a5196
bpfree <brian.patrick.free@gmail.com>
2024-10-08 10:11:20 -0400

    move parameters; update pathnames and standardize object names

--- code/21_cod_spawning.R
+++ code/21_cod_spawning.R
@@ -42,6 +42,23 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
+# set parameters
+## designate region name
+region_name <- "westport"
+
+## coordinate reference system
+### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
+crs <- "EPSG:26918"
+
+## layer names
+layer_name <- "cod_spawning"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
 # set directories
 ## define data directory (as this is an R Project, pathnames are simplified)
 ### input directories
@@ -49,37 +66,20 @@ pacman::p_load(docxtractr,
 data_dir <- "data/a_raw_data/cod_spawning_protection_areas/GOM_Spawning_Groundfish_Closures"
 
 #### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
 
 ### output directories
 #### fisheries
 fisheries_gpkg <- "data/c_submodel_data/fisheries.gpkg"
 
 #### intermediate directories
-cod_spawning_gpkg <- "data/b_intermediate_data/westport_cod_spawning.gpkg"
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
 
 #####################################
 
 # inspect layers within geopackage
-sf::st_layers(dsn = study_region_gpkg,
+sf::st_layers(dsn = region_gpkg,
               do_count = T)
-
-#####################################
-#####################################
-
-# set parameters
-## designate region name
-region <- "westport"
-
-## coordinate reference system
-### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
-crs <- "EPSG:26918"
-
-## layer names
-export_name <- "cod_spawning"
-
-## designate date
-date <- format(Sys.Date(), "%Y%m%d")
 
 #####################################
 #####################################
@@ -96,10 +96,10 @@ cod_spawning <- sf::st_read(dsn = data_dir,
 #####################################
 
 ## study region
-westport_region <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area", sep = "_"))
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
 
 ## hex grid
-westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area_hex", sep = "_"))
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
 
 #####################################
 #####################################
@@ -108,7 +108,7 @@ westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area
 westport_cod_spawning <- cod_spawning %>%
   # obtain only cod spawning protection areas in the study area
   rmapshaper::ms_clip(target = .,
-                      clip = westport_region) %>%
+                      clip = region) %>%
   # create field called "layer" and fill with "cod spawning protection areas" for summary
   dplyr::mutate(layer = "cod spawning protection areas")
 
@@ -116,7 +116,7 @@ westport_cod_spawning <- cod_spawning %>%
 #####################################
 
 # cod spawning protection areas hex grids
-westport_cod_spawning_hex <- westport_hex[westport_cod_spawning, ] %>%
+westport_cod_spawning_hex <- hex_grid[westport_cod_spawning, ] %>%
   # spatially join cod spawning protection areas values to Westport hex cells
   sf::st_join(x = .,
               y = westport_cod_spawning,
@@ -129,11 +129,11 @@ westport_cod_spawning_hex <- westport_hex[westport_cod_spawning, ] %>%
 
 # export data
 ## constraints geopackage
-sf::st_write(obj = westport_cod_spawning_hex, dsn = fisheries_gpkg, layer = paste(region, "hex", export_name, date, sep = "_"), append = F)
+sf::st_write(obj = westport_cod_spawning_hex, dsn = fisheries_gpkg, layer = stringr::str_glue("{region}_hex_{layer_name}_{date}"), append = F)
 
 ## cod spawning protection areas geopackage
-sf::st_write(obj = cod_spawning, dsn = cod_spawning_gpkg, layer = paste(export_name, date, sep = "_"), append = F)
-sf::st_write(obj = westport_cod_spawning, dsn = cod_spawning_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+sf::st_write(obj = cod_spawning, dsn = cod_spawning_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = westport_cod_spawning, dsn = cod_spawning_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
