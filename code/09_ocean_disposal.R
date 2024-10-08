264d1c3106f938df1b8e46726e5c3e140367a110
bpfree <brian.patrick.free@gmail.com>
2024-10-08 10:02:00 -0400

    move parameters; update pathnames and standardize object names

--- code/09_ocean_disposal.R
+++ code/09_ocean_disposal.R
@@ -42,44 +42,19 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
-# set directories
-## define data directory (as this is an R Project, pathnames are simplified)
-### input directories
-#### ocean disposal sites
-data_dir <- "data/a_raw_data/OceanDisposalSite/OceanDisposalSite.gpkg"
-
-#### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
-
-### output directories
-#### constraints
-constraints_gpkg <- "data/c_submodel_data/constraints.gpkg"
-
-#### intermediate directories
-ocean_disposal_gpkg <- "data/b_intermediate_data/westport_ocean_disposal.gpkg"
-
-#####################################
-
-# inspect layers within geodatabases and geopackages
-sf::st_layers(dsn = data_dir,
-              do_count = T)
-
-sf::st_layers(dsn = study_region_gpkg,
-              do_count = T)
-
-#####################################
-#####################################
-
 # set parameters
 ## designate region name
-region <- "westport"
+region_name <- "westport"
 
 ## coordinate reference system
 ### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
 crs <- "EPSG:26918"
 
 ## layer names
-export_name <- "ocean_disposal"
+layer_name <- "ocean_disposal"
+
+## submodel
+submodel <- "constraints"
 
 ## designate date
 date <- format(Sys.Date(), "%Y%m%d")
@@ -87,10 +62,38 @@ date <- format(Sys.Date(), "%Y%m%d")
 #####################################
 #####################################
 
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### ocean disposal sites
+data_dir <- "data/a_raw_data/OceanDisposalSite/OceanDisposalSite.gpkg"
+
+#### study area grid
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
+
+### output directories
+#### submodel
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
 # load data
 ## ocean disposal sites data (source: https://marinecadastre.gov/downloads/data/mc/OceanDisposalSite.zip)
 ### metadata: https://www.fisheries.noaa.gov/inport/item/54193
-ocean_disposal <- sf::st_read(dsn = data_dir,
+data <- sf::st_read(dsn = data_dir,
                            # ocean disposal site
                            layer = sf::st_layers(data_dir)[[1]][1]) %>%
   # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
@@ -99,19 +102,19 @@ ocean_disposal <- sf::st_read(dsn = data_dir,
 #####################################
 
 ## study region
-westport_region <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area", sep = "_"))
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
 
 ## hex grid
-westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area_hex", sep = "_"))
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
 
 #####################################
 #####################################
 
 # limit data to study region
-westport_ocean_disposal <- ocean_disposal %>%
+region_data <- data %>%
   # obtain only ocean disposal sites in the study area
   rmapshaper::ms_clip(target = .,
-                      clip = westport_region) %>%
+                      clip = region) %>%
   # create field called "layer" and fill with "ocean disposal" for summary
   dplyr::mutate(layer = "ocean disposal")
 
@@ -119,10 +122,10 @@ westport_ocean_disposal <- ocean_disposal %>%
 #####################################
 
 # ocean disposal sites hex grids
-westport_ocean_disposal_hex <- westport_hex[westport_ocean_disposal, ] %>%
+region_data_hex <- hex_grid[region_data, ] %>%
   # spatially join ocean disposal sites values to Westport hex cells
   sf::st_join(x = .,
-              y = westport_ocean_disposal,
+              y = region_data,
               join = st_intersects) %>%
   # select fields of importance
   dplyr::select(index, layer)
@@ -131,12 +134,12 @@ westport_ocean_disposal_hex <- westport_hex[westport_ocean_disposal, ] %>%
 #####################################
 
 # export data
-## constraints geopackage
-sf::st_write(obj = westport_ocean_disposal_hex, dsn = constraints_gpkg, layer = paste(region, "hex", export_name, date, sep = "_"), append = F)
+## submodel geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region_name}_hex_{layer_name}_{date}"), append = F)
 
-## ocean disposal sites geopackage
-sf::st_write(obj = ocean_disposal, dsn = ocean_disposal_gpkg, layer = paste(export_name, date, sep = "_"), append = F)
-sf::st_write(obj = westport_ocean_disposal, dsn = ocean_disposal_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+## data geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
