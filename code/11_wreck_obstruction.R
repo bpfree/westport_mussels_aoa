c7becb5cc1cfc2c8fb8e09dd19cc2c153c0af84e
bpfree <brian.patrick.free@gmail.com>
2024-10-08 10:02:21 -0400

    move parameters; update pathnames and standardize object names

--- code/11_wreck_obstruction.R
+++ code/11_wreck_obstruction.R
@@ -42,44 +42,19 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
-# set directories
-## define data directory (as this is an R Project, pathnames are simplified)
-### input directories
-#### wrecks and obstructions sites
-data_dir <- "data/a_raw_data/WreckObstruction/WreckObstruction.gpkg"
-
-#### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
-
-### output directories
-#### constraints
-constraints_gpkg <- "data/c_submodel_data/constraints.gpkg"
-
-#### intermediate directories
-wreck_obstruction_gpkg <- "data/b_intermediate_data/westport_wreck_obstruction.gpkg"
-
-#####################################
-
-# inspect layers within geodatabases and geopackages
-sf::st_layers(dsn = data_dir,
-              do_count = T)
-
-sf::st_layers(dsn = study_region_gpkg,
-              do_count = T)
-
-#####################################
-#####################################
-
 # set parameters
 ## designate region name
-region <- "westport"
+region_name <- "westport"
 
 ## coordinate reference system
 ### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
 crs <- "EPSG:26918"
 
 ## layer names
-export_name <- "wreck_obstruction"
+layer_name <- "wreck_obstruction"
+
+## submodel
+submodel <- "constraints"
 
 ## setback distance (in meters)
 setback <- 152.4
@@ -90,10 +65,38 @@ date <- format(Sys.Date(), "%Y%m%d")
 #####################################
 #####################################
 
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### wrecks and obstructions sites
+data_dir <- "data/a_raw_data/WreckObstruction/WreckObstruction.gpkg"
+
+#### study area grid
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
+
+### output directories
+#### submodel
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
 # load data
 ## wreck and obstruction data (source: https://marinecadastre.gov/downloads/data/mc/WreckObstruction.zip)
 ### metadata: https://www.fisheries.noaa.gov/inport/item/70439
-wreck_obstruction <- sf::st_read(dsn = data_dir,
+data <- sf::st_read(dsn = data_dir,
                                # wreck and obstruction
                                layer = sf::st_layers(data_dir)[[1]][1]) %>%
   # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
@@ -104,19 +107,19 @@ wreck_obstruction <- sf::st_read(dsn = data_dir,
 #####################################
 
 ## study region
-westport_region <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area", sep = "_"))
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
 
 ## hex grid
-westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area_hex", sep = "_"))
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
 
 #####################################
 #####################################
 
 # limit data to study region
-westport_wreck_obstruction <- wreck_obstruction %>%
+region_data <- data %>%
   # obtain only wreck and obstruction in the study area
   rmapshaper::ms_clip(target = .,
-                      clip = westport_region) %>%
+                      clip = region) %>%
   # create field called "layer" and fill with "wreck and obstruction" for summary
   dplyr::mutate(layer = "wreck and obstruction")
 
@@ -124,10 +127,10 @@ westport_wreck_obstruction <- wreck_obstruction %>%
 #####################################
 
 # wreck and obstruction hex grids
-westport_wreck_obstruction_hex <- westport_hex[westport_wreck_obstruction, ] %>%
+region_data_hex <- hex_grid[region_data, ] %>%
   # spatially join wreck and obstruction values to Westport hex cells
   sf::st_join(x = .,
-              y = westport_wreck_obstruction,
+              y = region_data,
               join = st_intersects) %>%
   # select fields of importance
   dplyr::select(index, layer) %>%
@@ -140,12 +143,12 @@ westport_wreck_obstruction_hex <- westport_hex[westport_wreck_obstruction, ] %>%
 #####################################
 
 # export data
-## constraints geopackage
-sf::st_write(obj = westport_wreck_obstruction_hex, dsn = constraints_gpkg, layer = paste(region, "hex", export_name, date, sep = "_"), append = F)
+## submodel geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region_name}_hex_{layer_name}_{date}"), append = F)
 
-## wreck and obstruction geopackage
-sf::st_write(obj = wreck_obstruction, dsn = wreck_obstruction_gpkg, layer = paste(export_name, date, sep = "_"), append = F)
-sf::st_write(obj = westport_wreck_obstruction, dsn = wreck_obstruction_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+## data geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
