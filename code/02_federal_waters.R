4c636a2614c5d8526ccf49201b1865fafa3887fd
bpfree <brian.patrick.free@gmail.com>
2024-10-08 09:51:08 -0400

    move parameters; update pathnames and standardize object names

--- code/02_federal_waters.R
+++ code/02_federal_waters.R
@@ -42,6 +42,23 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
+# set parameters
+## designate region name
+region_name <- "westport"
+
+## coordinate reference system
+### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
+crs <- "EPSG:26918"
+
+## layer names
+layer_name <- "federal_waters"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
 # set directories
 ## define data directory (as this is an R Project, pathnames are simplified)
 ### input directories
@@ -49,14 +66,14 @@ pacman::p_load(docxtractr,
 data_dir <- "data/a_raw_data/CoastalZoneManagementAct/CoastalZoneManagementAct.gpkg"
 
 #### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
 
 ### output directories
 #### constraints
 constraints_gpkg <- "data/c_submodel_data/constraints.gpkg"
 
 #### intermediate directories
-federal_waters_gpkg <- "data/b_intermediate_data/westport_federal_waters.gpkg"
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
 
 #####################################
 
@@ -64,25 +81,8 @@ federal_waters_gpkg <- "data/b_intermediate_data/westport_federal_waters.gpkg"
 sf::st_layers(dsn = data_dir,
               do_count = T)
 
-sf::st_layers(dsn = study_region_gpkg,
+sf::st_layers(dsn = region_gpkg,
               do_count = T)
-
-#####################################
-#####################################
-
-# set parameters
-## designate region name
-region <- "westport"
-
-## coordinate reference system
-### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
-crs <- "EPSG:26918"
-
-## layer names
-export_name <- "federal_waters"
-
-## designate date
-date <- format(Sys.Date(), "%Y%m%d")
 
 #####################################
 #####################################
@@ -104,11 +104,11 @@ federal_waters <- sf::st_read(dsn = data_dir,
 
 # export data
 ## constraints geopackage
-sf::st_write(obj = federal_waters, dsn = constraints_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
-sf::st_write(obj = federal_waters, dsn = study_region_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+sf::st_write(obj = federal_waters, dsn = constraints_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
+sf::st_write(obj = federal_waters, dsn = region_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 ## federal waters geopackage
-sf::st_write(obj = federal_waters, dsn = federal_waters_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+sf::st_write(obj = federal_waters, dsn = datagpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
