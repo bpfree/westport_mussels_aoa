be8bcc79a97c415de89f86bcf551d3ebc80b3997
bpfree <brian.patrick.free@gmail.com>
2024-10-08 10:02:09 -0400

    move parameters; update pathnames and standardize object names

--- code/10_aids_navigation.R
+++ code/10_aids_navigation.R
@@ -42,44 +42,19 @@ pacman::p_load(docxtractr,
 #####################################
 #####################################
 
-# set directories
-## define data directory (as this is an R Project, pathnames are simplified)
-### input directories
-#### aids to navigation sites
-data_dir <- "data/a_raw_data/AtoN/AtoN.gpkg"
-
-#### study area grid
-study_region_gpkg <- "data/b_intermediate_data/westport_study_area.gpkg"
-
-### output directories
-#### constraints
-constraints_gpkg <- "data/c_submodel_data/constraints.gpkg"
-
-#### intermediate directories
-aids_navigation_gpkg <- "data/b_intermediate_data/westport_aids_navigation.gpkg"
-
-#####################################
-
-# inspect layers within geodatabases and geopackages
-sf::st_layers(dsn = data_dir,
-              do_count = T)
-
-sf::st_layers(dsn = study_region_gpkg,
-              do_count = T)
-
-#####################################
-#####################################
-
 # set parameters
 ## designate region name
-region <- "westport"
+region_name <- "westport"
 
 ## coordinate reference system
 ### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
 crs <- "EPSG:26918"
 
 ## layer names
-export_name <- "aids_navigation"
+layer_name <- "aids_navigation"
+
+## submodel
+submodel <- "constraints"
 
 ## setback distance (in meters)
 setback <- 500
@@ -90,10 +65,38 @@ date <- format(Sys.Date(), "%Y%m%d")
 #####################################
 #####################################
 
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### aids to navigation sites
+data_dir <- "data/a_raw_data/AtoN/AtoN.gpkg"
+
+#### study area grid
+region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_study_area.gpkg")
+
+### output directories
+#### submodel
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region_name}_{layer_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
 # load data
 ## aids to navigation data (source: https://marinecadastre.gov/downloads/data/mc/AtoN.zip)
 ### metadata: https://www.fisheries.noaa.gov/inport/item/56120
-aids_navigation <- sf::st_read(dsn = data_dir,
+data <- sf::st_read(dsn = data_dir,
                               # aids to navigation site
                               layer = sf::st_layers(data_dir)[[1]][1]) %>%
   # change to correct coordinate reference system (EPSG:26918 -- NAD83 / UTM 18N)
@@ -104,19 +107,19 @@ aids_navigation <- sf::st_read(dsn = data_dir,
 #####################################
 
 ## study region
-westport_region <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area", sep = "_"))
+region <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area"))
 
 ## hex grid
-westport_hex <- sf::st_read(dsn = study_region_gpkg, layer = paste(region, "area_hex", sep = "_"))
+hex_grid <- sf::st_read(dsn = region_gpkg, layer = stringr::str_glue("{region_name}_area_hex"))
 
 #####################################
 #####################################
 
 # limit data to study region
-westport_aids_navigation <- aids_navigation %>%
+region_data <- data %>%
   # obtain only aids to navigation in the study area
   rmapshaper::ms_clip(target = .,
-                      clip = westport_region) %>%
+                      clip = region) %>%
   # create field called "layer" and fill with "aids to navigation" for summary
   dplyr::mutate(layer = "aids to navigation")
 
@@ -124,10 +127,10 @@ westport_aids_navigation <- aids_navigation %>%
 #####################################
 
 # aids to navigation hex grids
-westport_aids_navigation_hex <- westport_hex[westport_aids_navigation, ] %>%
+region_data_hex <- hex_grid[region_data, ] %>%
   # spatially join aids to navigation values to Westport hex cells
   sf::st_join(x = .,
-              y = westport_aids_navigation,
+              y = region_data,
               join = st_intersects) %>%
   # select fields of importance
   dplyr::select(index, layer)
@@ -136,12 +139,12 @@ westport_aids_navigation_hex <- westport_hex[westport_aids_navigation, ] %>%
 #####################################
 
 # export data
-## constraints geopackage
-sf::st_write(obj = westport_aids_navigation_hex, dsn = constraints_gpkg, layer = paste(region, "hex", export_name, date, sep = "_"), append = F)
+## submodel geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region_name}_hex_{layer_name}_{date}"), append = F)
 
-## aids to navigation geopackage
-sf::st_write(obj = aids_navigation, dsn = aids_navigation_gpkg, layer = paste(export_name, date, sep = "_"), append = F)
-sf::st_write(obj = westport_aids_navigation, dsn = aids_navigation_gpkg, layer = paste(region, export_name, date, sep = "_"), append = F)
+## data geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{layer_name}_{date}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{layer_name}_{date}"), append = F)
 
 #####################################
 #####################################
